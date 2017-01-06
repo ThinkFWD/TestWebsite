ee7f74adb1426ceb4908c3074d1349b7c11dd5a4
ThinkFWD <mike.thinkfwd@gmail.com>
2017-01-05 20:53:11 -0500

    Sample Javascript

---
+++ studynotes.js
@@ -0,0 +1,44 @@
+/* 
+================
+Prerequisits
+================
+
+Q: Why have values? 
+A: When you run a program, you need a method to track various types of data. Values are small units of data 
+   that the program keeps reack of by storing it into the computers hardware memory. (RAM)
+
+Q: Why have types? 
+A: It allows interpreters (Javascript) to assign special rules and toolkits pertaining to them. 
+
+Operators = +, -, % .. ect. 
+Expressions = 3+1 
+Statement = ends an expression with ";"
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
